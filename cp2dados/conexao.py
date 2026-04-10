e696c7305c60f8ff67c9de43bfc6912329e06f67
juliamenezesf <juliakauanemenezesf@gmail.com>
2026-04-10 16:50:18 -0300

    feat: adiciona variaveis de ambiente com fallback na conexao

--- cp2dados/conexao.py
+++ cp2dados/conexao.py
@@ -4,9 +4,9 @@ import oracledb
 def conectar():
     try:
         connection = oracledb.connect(
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            dsn=os.getenv("DB_DSN")
+            user=os.getenv("DB_USER") or "SEU_USUARIO",
+            password=os.getenv("DB_PASSWORD") or "SUA_SENHA",
+            dsn=os.getenv("DB_DSN") or "oracle.fiap.com.br:1521/ORCL"
         )
 
         print("Conectado com sucesso!")
